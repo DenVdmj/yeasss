9fdfb85d87fd55cc8a72a149d0d7a2d7ba2ec769
sunny.dreamwind <sunny.dreamwind@e6b1b8d2-c528-11dd-927d-974589228060>
2008-12-10 22:21:33 +0000

    Code refactored: simple cases moved to inner methods. Global doc variables moved to properties. Fast select speeded up (with methods). Small bug with :last-child detected, need to be fixed.

--- trunk/yass.js
+++ trunk/yass.js
@@ -7,7 +7,7 @@
 * and GPL (GPL-LICENSE.txt) licenses.
 *
 * $Date: 2008-12-09 23:00:11 +3000 (Tue, 9 Dec 2008) $
-* $Rev: 130 $
+* $Rev: 173 $
 */
 var _ = function () {
 /* given CSS selector, first argument, fast trim */
@@ -16,300 +16,119 @@ var _ = function () {
 	if (_.cache[selector] && !arguments[2]) {
 		return _.cache[selector];
 	} else {
-		if (!selector) {
-			return null;
-		}
 /* subtree added, second argument, thx to tenshi */
-		var root = arguments[1] || document;
-/*
-apply querySelector if exists.
+		var root = arguments[1] || _.doc;
+/* select first letter to fast switch in simple cases */
+		if (!/^.\w+$/.test(selector) || !(_.sets = _.simple[selector.charAt(0).replace(/[a-zA-Z\*]/,"%")](selector, root))) {
+/* all other cases.
+Apply querySelector if exists.
 All methods are called via . not [] - thx to arty
 */
-		if (root.querySelectorAll) {
-			_.sets = root.querySelectorAll(selector);
-		} else {
-			switch (selector) {
-/* return some simple and quick cases */
-				case 'a':
-					_.sets = root.links || root.getElementsByTagName('a');
-					break;
-				case 'body':
-					_.sets = root.body;
-					break;
-				case 'div':
-					_.sets = root.getElementsByTagName('div');
-					break;
-				case 'form':
-					_.sets = root.forms || root.getElementsByTagName('form');
-					break;
-				case 'h1':
-					_.sets = root.getElementsByTagName('h1');
-					break;
-				case 'h2':
-					_.sets = root.getElementsByTagName('h2');
-					break;
-				case 'h3':
-					_.sets = root.getElementsByTagName('h3');
-					break;
-				case 'head':
-					_.sets = root.getElementsByTagName('head')[0];
-					break;
-				case 'img':
-					_.sets = root.images || root.getElementsByTagName('img');
-					break;
-				case 'p':
-					_.sets = root.getElementsByTagName('p');
-					break;
-				case 'table':
-					_.sets = root.getElementsByTagName('table');
-					break;
-				case 'title':
-					_.sets = root.getElementsByTagName('title')[0];
-					break;
-/* generic case */
-				default:
-					var doc = document,
-						isSingle = !/\s|,/.test(selector);
-/* return some simple cases: only ID or only CLASS */
-					switch (selector[0]) {
-						case '#':
-							if (isSingle) {
-								_.sets = _.getUniqueId(doc, selector.slice(1));
-								break;
-							}
-						case '.':
-							if (isSingle && root.getElementsByClassName(selector.slice(1))) {
-								_.sets = root.getElementsByClassName(selector.slice(1));
-								break;
-							}
-/* all other cases */
-						default:
+			if (root.querySelectorAll) {
+				_.sets = root.querySelectorAll(selector);
+			} else {
+/* number of groups to merge or not result arrays */
+				var groups_length = 0,
 /*
 groups of selectors separated by commas.
 Split by RegExp, thx to tenshi.
 */
-							var groups = selector.split(/, */),
-								group,
-								j = 0;
-							while (group = groups[j++]) {
+					groups = selector.split(/,\s*/),
+					group;
+				while (group = groups[groups_length++]) {
 /* split selectors by space -- to form single group tag-id-class */
-								var singles = group.split(/ +/),
-									singles_length = singles.length,
-									single,
-									i = 0;
+					var singles = group.split(/\s+/),
+						singles_length = singles.length,
+						single,
+						i = 0;
 /* clean nodes with DOM root */
-								_.nodes = root;
-								while (single = singles[i++]) {
+					_.nodes = root;
+					while (single = singles[i++]) {
 /*
 inspired with John's Resig fast replace implementation, more details:
 http://ejohn.org/blog/search-and-dont-replace/
 http://webo.in/articles/habrahabr/40-search-not-replace/
 thx to GreLI for 'greed' RegExp
 */
-									single.replace(/([^\[\:\.#]+)?(?:#([^\[\:\.#]+))?(?:\.([^\[\:\.#]+))?(?:\[([^\[\:\.#]+)=([^\[\:\.#]+)\])?(?:\:([^\(\[\:\.#]+)(?:\(([^\)]+)\))?)?/, function(a, tag, id, klass, attr, value, modificator, ind) {
+						single.replace(/([^\s\[\:\.#]+)?(?:#([^\s\[\:\.#]+))?(?:\.([^\s\[\:\.#]+))?(?:\[([^\s\[\:\.#]+)=([^\s\[\:\.#]+)\])?(?:\:([^\s\(\[\:\.#]+)(?:\(([^\)]+)\))?)?/, function(a, tag, id, klass, attr, value, modificator, ind) {
 /*
 switch to quick select for the root node.
 There won't be any duplicates as far as it's the first level
 */
-										switch (i) {
-											case 0:
-												if (!modificator) {
-/* perform quick check for ID, won't work always correctly if ROOT is set */
-													if (!klass) {
-														if (!tag) {
-/*
-usage of cached document isn't faster in all browsers,
-due to optimized calls for document.getElementById,
-but we are working for IE. Thx to GreLI
-*/
-															_.nodes = _.getUniqueId(doc, id);
-/* check if element has given attribute with value */	
-															if (!attr || _.nodes[attr] === value) {
-																break;
-															}
-														}
-/* quick check for TAG */
-														if (!id && !attr) {
-															_.nodes = root.getElementsByTagName(tag);
-															break;
-														}
-													}
-/* quick check for CLASS */
-													if (!tag && !id && !attr && !modificator && root.getElementsByClassName) {
-														_.nodes = root.getElementsByClassName(klass);
-														break;
-													}
-												}
-/* generic selector to get element by TAG, CLASS, ID, ATTR/VALUE, MIDIFICATOR */
-											default:
-/* array to merge results */
-												var newNodes = [],
+							var newNodes = [],
 /* length of root nodes */
-													J = 0,
+								J = 0,
 /* iterator of return array, equals to its length */
-													idx = 0,
-													node;
+								idx = 0,
+								node;
 /*
 if root is single -- just make it as an array. Cached
 variable is a bit faster in Fx3, but definitely slower in Opera9.
 */
-												_.nodes = _.nodes.length ? _.nodes : [_.nodes];
+							_.nodes = _.nodes.length ? _.nodes : [_.nodes];
 /* loop in all root nodes */
-												while (node = _.nodes[J++]) {
-													var h = 0,
-														child,
+							while (node = _.nodes[J++]) {
+								var h = 0,
+									child,
 /* find all TAGs */
-													childs = node.getElementsByTagName(tag || '*');
-													while (child = childs[h++]) {
+									childs = node.getElementsByTagName(tag || '*');
+								while (child = childs[h++]) {
 /*
 check them for ID or Class. Also check for expando 'yeasss'
 to filter non-selected elements. Typeof 'string' not added -
 if we get element with name="id" it won't be equal to given ID.
 */
-														if ((!id || (id && child.id === id)) && (!klass || (klass && child.className.match(klass))) && (!attr || (attr && child[attr] === value) || (attr === 'class' && child.className.match(value))) && !child.yeasss) {
-/*
-CSS2/3 modificatos. Specification taken from
-http://www.w3.org/TR/2005/WD-css3-selectors-20051215/
-in success just null the flag -- this will be equal to
-the ordinary case. Don't null a modificator -- it can
-be used for other loops.
-*/
-															var flag = modificator;
-															switch (flag) {
-/* from w3.org: "an E element, first child of its parent" */
-																case 'first-child':
-/* implementation was taken from jQuery.1.2.6, line 1394 */
-																	if (child.parentNode.getElementsByTagName('*')[0] === child) {
-																		flag = 0;
-																	}
-																	break;
-/* from w3.org: "an E element, last child of its parent" */
-																case 'last-child':
-																	var brother = child.parentNode.lastChild;
-/* pre-check. Will be true if most normal cases */
-																	if (brother === child) {
-																		flag = 0;
-																	} else {
-/* loop in siblings back while nodeType isn't element */
-																		while ((brother = brother.previousSibling) && brother.nodeType != 1) {
-																		}
-/* post check. Maybe true in some cases */
-																		if (brother === child) {
-																			flag = 0;
-																		}
-																	}
-																	break;
-/* from w3.org: "an E element, root of the document" */
-																case 'root':
-																	if (child.nodeName.toLowerCase() === 'html') {
-																		flag = 0;
-																	}
-																	break;
-/*
-from w3.org: "an E element, the n-th child of its parent"
-Completely wrong at this moment. Need to support at least: n, 2n, 2n+1
-*/
-																case 'nth-child':
-																	if (ind > -1 && child.parentNode.getElementsByTagName('*')[ind] === child) {
-																		flag = 0;
-																	}
-																	break;
-/*
-from w3.org: "an E element, the n-th child of its parent,
-counting from the last one"
-*/
-																case 'nth-last-child':
-																	var brothers = child.parentNode.getElementsByTagName('*');
-																	if (ind > -1 && brothers[brothers.length - 1 - ind] === child) {
-																		flag = 0;
-																	}
-																	break;
-/* from w3.org: "an E element that has no children (including text nodes)" */
-																case 'empty':
-																	if (!child.hasChildNodes()) {
-																		flag = 0;
-																	}
-																	break;
-/* from w3.org: "an E element, only child of its parent" */
-																case 'only-child':
-																	if (child.parentNode.getElementsByTagName('*').length == 1) {
-																		flag = 0;
-																	}
-																	break;
-/*
-from w3.org: "a user interface element E which is checked
-(for instance a radio-button or checkbox)"
-*/
-																case 'checked':
-																	if (child.nodeName.toLowerCase() === 'input' && (child.type === 'checkbox' || child.type === 'radio') && child.checked) {
-																		flag = 0;
-																	}
-																	break;
-/*
-from w3.org: "an element of type E in language "fr"
-(the document language specifies how language is determined)"
-*/
-																case 'lang':
-																	if (child.lang === ind || doc.getElementsByTagName('html')[0].lang === ind) {
-																		flag = 0;
-																	}
-																	break;
-															}
+									if ((!id || (id && child.id === id)) && (!klass || (klass && child.className.match(klass))) && (!attr || (attr && child[attr] === value) || (attr === 'class' && child.className.match(value))) && !child.yeasss) {
 /*
 modificator is either not set in the selector,
 or just has been nulled by previous switch
 */
-															if (!flag) {
+										if (!(_.modificators[modificator] ? _.modificators[modificator](child, ind) : modificator)) {
 /* 
 Need to define expando property to true for the last step.
 Then mark selected element with expando
 */
-																if (i == singles_length) {
-																	child.yeasss = 1;
-																}
+											if (i == singles_length) {
+												child.yeasss = 1;
+											}
 /* and add to result array */
-																newNodes[idx++] = child;
-															}
-														}
-													}
-												}
-/* put selected nodes in local nodes' set */
-												_.nodes = newNodes ? newNodes : null;
-												break;
+											newNodes[idx++] = child;
 										}
-									});
-								}
-/* inialize sets with nodes */
-								_.sets = _.sets || _.nodes;
-/* fixing bug on non-existent selector, thx to deerua */
-								if (_.nodes && j > 1) {
-									var node,
-										K = 0,
-										idx = _.sets.length;
-/* remember selected nodes to global set to start new selection */
-									while (node = _.nodes[K++]) {
-										_.sets[idx++] = node;
 									}
 								}
 							}
-							var idx = _.sets.length;
+/* put selected nodes in local nodes' set */
+							_.nodes = newNodes && newNodes.length ? newNodes : null;
+						});
+					}
+/* inialize sets with nodes */		
+					_.sets = _.sets || _.nodes;
+/* fixing bug on non-existent selector, thx to deerua */
+					if (_.nodes && groups_length > 1) {
+						var node,
+							K = 0,
+							idx = _.sets.length;
+/* remember selected nodes to global set to start new selection */
+						while (node = _.nodes[K++]) {
+							_.sets[idx++] = node;
+						}
+					}
+				}
+				var idx = _.sets ? _.sets.length : 0;
 /*
 Need this looping as far as we also have expando 'yeasss'
 that must be nulled. Need this only to non-default case
 */
-							while (idx--) {
-								_.sets[idx].yeasss = null;
-							}
-							break;
-					}
-					break;
+				while (idx--) {
+					_.sets[idx].yeasss = null;
+				}
 			}
 		}
 /* save result in cache */
 		_.cache[selector] = _.sets ? _.sets.length || _.sets.nodeName ? _.sets : null : null;
 /* clear all properties to prevent memory leaks */
 		_.sets = _.nodes = null;
-/* return saved result */
+/* return result */
 		return _.cache[selector];
 	}
 };
@@ -319,34 +138,137 @@ _.nodes = null;
 _.sets = null;
 /* cache for selected nodes, no leaks in IE detected */
 _.cache = {};
-/* method to return unique element by ID */
-_.getUniqueId = function(doc, id) {
-	var nodes = doc.getElementById(id);
+/* caching global document */
+_.doc = document;
+/*
+return some simple cases: only ID or only CLASS for the very
+first case: don't need additional checks
+*/
+_.simple = {
+	'#':
+		function (selector) {
+			var id = selector.slice(1),
+				nodes = _.doc.getElementById(id);
 /*
 workaround with IE bug about returning element by name not by ID.
 Modified solution from
 http://deer.org.ua/2008/08/15/2/
 thx to deerua. Get all matching elements with this id
 */
-	if (doc.all && node.id !== id && (nodes = doc.all[id])) {
-		var nodes_length = nodes.length;
+			if (_.doc.all && nodes.id !== id && (nodes = _.doc.all[id])) {
+				var nodes_length = nodes.length;
 /*
 if more than 1, choose first with the correct id.
 if we have the only element -- it's already in nodes.
 */
-		if (nodes_length) {
+				if (nodes_length) {
 /* loop in given elements to find the correct one */
-			while (nodes_length--) {
-				var node = nodes[nodes_length];
-				if (node.id === id) {
-					nodes = node;
-					nodes_length = 0;
+					while (nodes_length--) {
+						var node = nodes[nodes_length];
+						if (node.id === id) {
+							nodes = node;
+							nodes_length = 0;
+						}
+					}
 				}
 			}
+			return nodes;
+		},
+	'.':
+		function (selector, root) {
+			if (root.getElementsByClassName) {
+				return root.getElementsByClassName(selector.slice(1));
+			}
+			return null;
+		},
+	'%':
+		function (selector, root) {
+			return root.getElementsByTagName(selector);
+		},
+	'[':
+		function (selector, root) {},
+	':':
+		function (selector, root) {}
+};
+/*
+function calls for CSS2/3 modificatos. Specification taken from
+http://www.w3.org/TR/2005/WD-css3-selectors-20051215/
+in success just null the flag -- this will be equal to
+the ordinary case. Don't null a modificator -- it can
+be used for other loops.
+*/
+_.modificators = {
+/* from w3.org: "an E element, first child of its parent" */
+	'first-child':
+		function (child) {
+/* implementation was taken from jQuery.1.2.6, line 1394 */
+			return child.parentNode.getElementsByTagName('*')[0] !== child;
+		},
+/* from w3.org: "an E element, last child of its parent" */
+	'last-child':
+		function (child) {
+			var brother = child.parentNode.lastChild;
+/* pre-check. Will be true if most normal cases */
+			if (brother === child) {
+				return 0;
+			}
+/* loop in lastChilds while nodeType isn't element */
+			while ((brother = brother.previousSibling) && brother.nodeType != 1) {}
+/* post check. Maybe true in some cases */
+			if (brother === child) {
+				return 0;
+			}
+			return 1;
+		},
+/* from w3.org: "an E element, root of the document" */
+	'root':
+		function (child) {
+			return child.nodeName.toLowerCase() !== 'html';
+		},
+/*
+from w3.org: "an E element, the n-th child of its parent"
+Completely wrong at this moment. Need to support at least: n, 2n, 2n+1
+*/
+	'nth-child':
+		function (child, ind) {
+			return ind < 0 || child.parentNode.getElementsByTagName('*')[ind] !== child;
+		},
+/*
+from w3.org: "an E element, the n-th child of its parent,
+counting from the last one"
+*/
+	'nth-last-child':
+		function (child, ind) {
+			var brothers = child.parentNode.getElementsByTagName('*');
+			return ind < 0 || brothers[brothers.length - 1 - ind] !== child;
+		},
+/* from w3.org: "an E element that has no children (including text nodes)" */
+	'empty':
+		function (child) {
+			return child.hasChildNodes(); 
+		},
+/* from w3.org: "an E element, only child of its parent" */
+	'only-child':
+		function (child) {
+			return (child.parentNode.getElementsByTagName('*').length !== 1);
+		},
+/*
+from w3.org: "a user interface element E which is checked
+(for instance a radio-button or checkbox)"
+*/
+	'checked':
+		function (child) {
+			return (child.nodeName.toLowerCase() !== 'input' || !(child.type === 'checkbox' || child.type === 'radio') || !child.checked);
+		},
+/*
+from w3.org: "an element of type E in language "fr"
+(the document language specifies how language is determined)"
+*/
+	'lang':
+		function (child, ind) {
+			return (child.lang !== ind && document.getElementsByTagName('html')[0].lang !== ind);
 		}
-	}
-	return nodes;
-}
+};
 /* initialization as a global var */
 window.yass = _;
 /* do not override existing window._ */
