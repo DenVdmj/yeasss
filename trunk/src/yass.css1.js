73a21828526134b151d88f717e3b3999c8a92f70
sunny.dreamwind <sunny.dreamwind@e6b1b8d2-c528-11dd-927d-974589228060>
2009-02-02 15:36:12 +0000

    Experimental change of algorithm to reverse lookup. Faster than straight one by 10% (in Fx3, IE shows the same speed for CSS1 selectors, Opera/Safari use querySelectorAll - aren't affected).

--- trunk/src/yass.css1.js
+++ trunk/src/yass.css1.js
@@ -2,15 +2,15 @@
 /*
 * YASS1 - The fastest CSS1 selectors JavaScript library
 * Experimental branch of YASS - just CSS1 family.
-* Faster only by 20-30% than YASS 0.3.8
+* Faster only by 20-30% than YASS 0.4.0
 * Slower than native methods by 100-300%
 *
 * Copyright (c) 2008 Nikolay Matsievsky aka sunnybear (webo.in, webo.name)
 * Dual licensed under the MIT (MIT-LICENSE.txt)
 * and GPL (GPL-LICENSE.txt) licenses.
 *
-* $Date: 2009-02-02 15:53:29 +3000 (Mon, 02 Feb 2009) $
-* $Rev: 13 $
+* $Date: 2009-02-02 18:29:29 +3000 (Mon, 02 Feb 2009) $
+* $Rev: 15 $
 */
 /**
  * Returns number of nodes or an empty array
@@ -69,7 +69,7 @@ Get all matching elements with this id
 all other cases. Apply querySelector if exists.
 All methods are called via . not [] - thx to arty
 */
-		if (_.q) {
+		if (_.q && selector.indexOf('!=') == -1) {
 			return root.querySelectorAll(selector);
 /* generic function for complicated selectors */
 		} else {
@@ -84,24 +84,28 @@ Split by RegExp, thx to tenshi.
 /* sets of nodes, to handle comma-separated selectors */
 				sets = [],
 				singles,
-				singles_length,
 /* to handle RegExp for single selector */
 				single,
 				i,
 /* current set of nodes - to handle single selectors */
 				nodes,
-				tag, id, klass, newNodes, idx, J, cjild, last, childs, item, h;
+				newNodes, idx, J, child, last, childs, item, h, id, klass, tag, par,
+/* stack for reverse looping */
+				stack,
+/* remember is need to check root */
+				rootReq = root && root !== _.doc;
 /* loop in groups, maybe the fastest way */
 			while (group = groups[groups_length++]) {
 /*
-Split selectors by space - to form single group tag-id-class,
-or to get heredity operator. Replace + in child modificators
-to % to avoid collisions. Additional replace is required for IE.
+Split selectors by space - to form single group tag-id-class
 */
-				singles_length = (singles = group.split(/ +/)).length;
+				singles = group.split(/ +/);
+				newNodes = [];
+				stack = [];
+				h = 0;
+				idx = 0;
+				J = 0;
 				i = 0;
-/* is cleanded up with DOM root */
-				nodes = [root];
 /*
 John's Resig fast replace works a bit slower than
 simple exec. Thx to GreLI for 'greed' RegExp
@@ -109,60 +113,58 @@ simple exec. Thx to GreLI for 'greed' RegExp
 				while (single = singles[i++]) {
 					single = /([^[:.#]+)?(?:#([^[:.#]+))?(?:\.([^[:.]+))?(?:\[([^!~^*|$[:=]+)([!$^*|]?=)?([^:\]]+)?\])?(?:\:([^(]+)(?:\(([^)]+)\))?)?/.exec(single);
 /* 
-Get all required matches from exec:
-tag, id, class, attribute, value, modificator, index.
+Get all required matches from exec to stack:
+tag, id, class
 */
-					tag = single[1] || '*';
-					id = single[2];
-					klass = single[3] ? ' ' + single[3] + ' ' : '';
+					stack[h++] = [single[1] ? single[1].toLowerCase() : '', single[2], single[3] ? ' ' + single[3] + ' ' : ''];
+				}
+				i = h;
+				item = stack[i - 1];
+				tag = item[0];
+				id = item[1];
+				klass = item[2];
 /* new nodes array */
+				nodes = id ? [_.doc.getElementById(id)] : _.k && !tag && klass ? _.doc.getElementsByClassName(klass) : _.doc.getElementsByTagName(tag || '*');
+/* pre-check of selected nodes */
+				while (child = nodes[J++]) {
+					if ((!tag || child.nodeName.toLowerCase() === tag) && (!id || child.id === id) && (!klass || (' ' + child.className +' ').indexOf(klass) != -1)) {
+						newNodes[idx++] = child;
+					}
+				}
+				if (i--) {
+					nodes = newNodes;
+					idx = 0;
+					J = 0;
 					newNodes = [];
-/*
-cached length of new nodes array
-and length of root nodes
-*/
-					idx = J = 0;
-/* if we need to mark node with expando yeasss */
-					last = i === singles_length;
-/* loop in all root nodes */
+/* reverse looping through stack for all nodes */
 					while (child = nodes[J++]) {
-/*
-find all TAGs or just return all possible neibours.
-Find correct 'children' for given node. They can be
-direct childs, neighbours or something else.
-*/
-						childs = child.getElementsByTagName(tag);
-						h = 0;
-						while (item = childs[h++]) {
-/*
-check them for ID or Class. Also check for expando 'yeasss'
-to filter non-selected elements. Typeof 'string' not added -
-if we get element with name="id" it won't be equal to given ID string.
-Also check for given attributes selector.
-Modificator is either not set in the selector, or just has been nulled
-by modificator functions hash.
-*/
-							if ((!id || item.id === id) && (!klass || (' ' + item.className + ' ').indexOf(klass) != -1) && !item.yeasss) {
-/* 
-Need to define expando property to true for the last step.
-Then mark selected element with expando
-*/
-								if (last) {
-									item.yeasss = 1;
-								}
-								newNodes[idx++] = item;
+						h = i - 1;
+						par = child;
+/* climb up through parent nodes and stack */
+						while ((item = stack[h--]) && par && (par = par.parentNode)) {
+							tag = item[0];
+							id = item[1];
+							klass = item[2];
+							while (par && ((tag && par.nodeName.toLowerCase() !== tag) || (id && par.id !== id) || (klass && (' ' + par.className +' ').indexOf(klass) == -1))) {
+								par = par.parentNode;
 							}
 						}
+						if (rootReq) {
+							while ((par = par.parentNode) && par !== root) {}
+						}
+						if (child && par && !child.yeasss) {
+							child.yeasss = 1;
+							newNodes[idx++] = child;
+						}
 					}
-/* put selected nodes in local nodes' set */
-					nodes = newNodes;
 				}
-/* inialize sets with nodes */
-				sets = sets.length ? sets : nodes;
 /* fixing bug on non-existent selector, thx to deerua */
 				if (groups_length > 1) {
 /* concat is faster than simple looping */
-					sets = sets.concat(nodes);
+					sets = sets.concat(newNodes);
+				} else {
+/* inialize sets with nodes */
+					sets = newNodes;
 				}
 			}
 /* define sets length to clean yeasss */
@@ -172,7 +174,7 @@ Need this looping as far as we also have expando 'yeasss'
 that must be nulled. Need this only to generic case
 */
 			while (idx--) {
-				sets[idx].yeasss = sets[idx].nodeIndex = sets[idx].nodeIndexLast = null;
+				sets[idx].yeasss = null;
 			}
 /* return results */
 			return sets;
