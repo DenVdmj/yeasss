d7644aadbbb8472e8460c7bd0627b9cdc3f63117
sunny.dreamwind <sunny.dreamwind@e6b1b8d2-c528-11dd-927d-974589228060>
2009-06-06 20:25:34 +0000

    Added setTimeout to wait until YASS is loaded

--- modules/yass.unused.selectors.js
+++ modules/yass.unused.selectors.js
@@ -2,41 +2,47 @@
 	var script = document.createElement('script');
 	script.src = 'http://yeasss.googlecode.com/svn/trunk/yass/src/yass.js';
 	document.body.appendChild(script);
-	var sheets = _.doc.styleSheets,
-		sheet,
-		i = 0,
-		rules,
-		rule,
-		j,
-		text,
-		selector,
-		k,
-		log = [];
-	while (sheet = sheets[i++]) {
-		if (!sheet.disabled) {
-			j = 0;
-			rules = sheet.cssRules;
-			while (rule = rules[j++]) {
-				text = rule.cssText;
-				text = text.substring(0, text.indexOf('{'));
-				text = text.split(',');
-				k = 0;
-				while (selector = text[k++]) {
-					if (!_(selector.replace(/(^\s+|\s+$)/,""))[0]) {
-						log[log.length] = selector;
+	setTimeout(function() {
+		if (!_) {
+			arguments.callee;
+		} else {
+			var sheets = _.doc.styleSheets,
+				sheet,
+				i = 0,
+				rules,
+				rule,
+				j,
+				text,
+				selector,
+				k,
+				log = [];
+			while (sheet = sheets[i++]) {
+				if (!sheet.disabled) {
+					j = 0;
+					rules = sheet.cssRules;
+					while (rule = rules[j++]) {
+						text = rule.cssText;
+						text = text.substring(0, text.indexOf('{'));
+						text = text.split(',');
+						k = 0;
+						while (selector = text[k++]) {
+							if (!_(selector.replace(/(^\s+|\s+$)/,""))[0]) {
+								log[log.length] = selector;
+							}
+						}
 					}
 				}
 			}
+			var container = document.createElement('blockquote');
+			container = document.body.appentChild(container);
+			var style = container.style;
+			style.position = 'absolute';
+			style.left = '10px';
+			style.padding = '5px';
+			style.top = '10px;';
+			style.background = '#fff';
+			style.border = '1px solid #999';
+			container.innerHTML = '<pre>' + log.implode("\n") + '</pre>';
 		}
-	}
-	var container = document.createElement('blockquote');
-	container = document.body.appentChild(container);
-	var style = container.style;
-	style.position = 'absolute';
-	style.left = '10px';
-	style.padding = '5px';
-	style.top = '10px;';
-	style.background = '#fff';
-	style.border = '1px solid #999';
-	container.innerHTML = '<pre>' + log.implode("\n") + '</pre>';
+	});
 })()
