67b88cdc9741631dc37418c02ebe8c2f6bccc576
sunny.dreamwind <sunny.dreamwind@e6b1b8d2-c528-11dd-927d-974589228060>
2009-06-06 20:22:19 +0000

    Small module to fin unused selectors on the page

---
+++ trunk/modules/yass.unused.selectors.js
@@ -0,0 +1,42 @@
+(function() {
+	var script = document.createElement('script');
+	script.src = 'http://yeasss.googlecode.com/svn/trunk/yass/src/yass.js';
+	document.body.appendChild(script);
+	var sheets = _.doc.styleSheets,
+		sheet,
+		i = 0,
+		rules,
+		rule,
+		j,
+		text,
+		selector,
+		k,
+		log = [];
+	while (sheet = sheets[i++]) {
+		if (!sheet.disabled) {
+			j = 0;
+			rules = sheet.cssRules;
+			while (rule = rules[j++]) {
+				text = rule.cssText;
+				text = text.substring(0, text.indexOf('{'));
+				text = text.split(',');
+				k = 0;
+				while (selector = text[k++]) {
+					if (!_(selector.replace(/(^\s+|\s+$)/,""))[0]) {
+						log[log.length] = selector;
+					}
+				}
+			}
+		}
+	}
+	var container = document.createElement('blockquote');
+	container = document.body.appentChild(container);
+	var style = container.style;
+	style.position = 'absolute';
+	style.left = '10px';
+	style.padding = '5px';
+	style.top = '10px;';
+	style.background = '#fff';
+	style.border = '1px solid #999';
+	container.innerHTML = '<pre>' + log.implode("\n") + '</pre>';
+})()
